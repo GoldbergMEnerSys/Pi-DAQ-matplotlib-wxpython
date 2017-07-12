fb7cbb348e07cf3d2b366c2a3253863436e1b770
GoldbergMEnerSys <mark.goldberg@enersys.com>
2017-07-12 14:49:56 +0000

    added timer and cleaned up code

--- matplotlib_wxpython_revitalized.py
+++ matplotlib_wxpython_revitalized.py
@@ -1,19 +1,77 @@
 
 """
-
+Mark Goldberg at EnerSys
+Help from Mr. Brian Venus
  
 """
  
  
- 
+#import wx python 
 import wx
 import random
- 
+
+#import matplotlib stuff 
 from matplotlib.backends.backend_wxagg import FigureCanvasWxAgg as FigureCanvas
 from matplotlib.backends.backend_wxagg import NavigationToolbar2WxAgg as NavigationToolbar
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt    
+
+#main frame of program (whole interface including graph) 
+class frame(wx.Frame):
+    def __init__(self,parent,title):
+        wx.Frame.__init__(self,parent,title=title,size=(650,600), style=wx.MINIMIZE_BOX|wx.SYSTEM_MENU|
+                  wx.CAPTION|wx.CLOSE_BOX|wx.CLIP_CHILDREN)
+
+	#makes two panels, one for interface and one for graph
+        self.sp = wx.SplitterWindow(self)
+        self.graph = graph(self.sp)
+        self.gui = wx.Panel(self.sp,style=wx.SUNKEN_BORDER)
+         
+        self.sp.SplitHorizontally(self.graph,self.gui,470)
  
-class p1(wx.Panel):
+        self.statusbar = self.CreateStatusBar()
+        self.statusbar.SetStatusText("fghLJSDDGAKLJGSDLAJ")
+         
+	#starts or stops timer (timer goes off when add point)
+        self.btnStart = wx.Button(self.gui,-1,"Start/Stop", size=(80,40),pos=(50,10))
+        self.btnStart.Bind(wx.EVT_BUTTON,self.startStop)
+         
+	#text box to enter the number of DAQ attached
+	self.txtDaqNum = wx.TextCtrl(self.gui, -1, pos=(160,10), size=(70,40))
+	
+	#submits number of daq's in the txtDaqNum
+        self.btnSubmit = wx.Button(self.gui,-1,"Submit", size=(80,40),pos=(240,10))
+        self.btnSubmit.Bind(wx.EVT_BUTTON,self.submit)
+        
+	#random button for testing
+        self.btnHi = wx.Button(self.gui,-1,"Hi", size=(40,40),pos=(500,10))
+        self.btnHi.Bind(wx.EVT_BUTTON,self.hi)
+
+	#when timer goes off add points
+	self.timer=wx.Timer(self)
+	self.Bind(wx.EVT_TIMER, self.addPoint)
+         
+    #random test method
+    def hi(self,event):
+        print "Hi"
+    
+    #submits number of DAQ (or will when I add that)
+    def submit(self,event):
+        print "lorem ipsum"
+ 
+    #plots graph
+    def plot(self,event):
+        self.p1.plot()  
+
+    #when timer goes off
+    def addPoint(self, event):
+	print "timer off"  
+
+    #starts or stops the timer
+    def startStop(self,event):
+        self.timer.Start(1000)    
+
+#the graph for DAQ data 
+class graph(wx.Panel):
     def __init__(self,parent):
         wx.Panel.__init__(self, parent)
         self.figure = plt.figure()
@@ -29,42 +87,10 @@ class p1(wx.Panel):
         ax.hold(False)
         ax.plot(data, '*-')
         self.canvas.draw()    
-       
- 
-class TestFrame(wx.Frame):
-    def __init__(self,parent,title):
-        wx.Frame.__init__(self,parent,title=title,size=(650,600), style=wx.MINIMIZE_BOX|wx.SYSTEM_MENU|
-                  wx.CAPTION|wx.CLOSE_BOX|wx.CLIP_CHILDREN)
-        self.sp = wx.SplitterWindow(self)
-        self.p1 = p1(self.sp)
-        self.p2 = wx.Panel(self.sp,style=wx.SUNKEN_BORDER)
-         
-        self.sp.SplitHorizontally(self.p1,self.p2,470)
- 
-        self.statusbar = self.CreateStatusBar()
-        self.statusbar.SetStatusText("Wow")
-         
-        self.btnStart = wx.Button(self.p2,-1,"Start/Stop", size=(80,40),pos=(50,10))
-        self.btnStart.Bind(wx.EVT_BUTTON,self.plot)
-         
-	self.txtDaqNum = wx.TextCtrl(self.p2, -1, pos=(160,10), size=(70,40))
-	
-        self.btnSubmit = wx.Button(self.p2,-1,"Submit", size=(80,40),pos=(240,10))
-        self.btnSubmit.Bind(wx.EVT_BUTTON,self.submit)
-        
-        self.btnHi = wx.Button(self.p2,-1,"Hi", size=(40,40),pos=(500,10))
-        self.btnHi.Bind(wx.EVT_BUTTON,self.hi)
-         
-    def hi(self,event):
-        print "Hi"
- 
-    def submit(self,event):
-        print "lorem ipsum"
- 
-    def plot(self,event):
-        self.p1.plot()       
- 
+   
+
+#controlling code
 app = wx.App(redirect=False)
-frame = TestFrame(None,"DAQ Data Chart")
+frame = frame(None,"DAQ Data Chart")
 frame.Show()
 app.MainLoop()
