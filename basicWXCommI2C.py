fc0d57531be61e0ab97c355de85d7e720793231e
GoldbergMEnerSys <mark.goldberg@enersys.com>
2017-07-17 13:45:47 +0000

    wxPython now sends and recieves one set of data

--- basicWXCommI2C.py
+++ basicWXCommI2C.py
@@ -7,15 +7,13 @@ window = wx.Frame(None, title = "Frame", size = (500,500))
 panel = wx.Panel(window)
 label = wx.StaticText(panel, label = "Hello World", pos = (100,50))
 
+var = 66
 
-proc = subprocess.Popen("./i2cDigipot2 0x4", shell = True, stdout=subprocess.PIPE)
-
-print "hi"
+proc = subprocess.Popen("./i2cDigipot2 %i"%var, shell = True, stdout=subprocess.PIPE)
 
 
 for line in proc.stdout:
     print line
-    print "Check"
 
 
 print "done"
--- i2cDigipot2.cpp
+++ i2cDigipot2.cpp
@@ -8,34 +8,20 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-   //cout << argv << endl;
-   //cout << argv[1] << endl;
+
    int numValue  = atoi(argv[1]);
-   string word;
+   //std::stringstream stream;
 
-   //memcpy(word, argv[0])
-
-   //cout << num << endl;
-
-   //numValue = Integer.parseInt(word);
-   cout <<"hi " << numValue << endl;
-
-   /*for (int i=0; i < argv[0].len(); i++) {
-   	char* value = argv[i];
-        cout << value << endl;
-   	numValue += (*value%48)*(10^(i-1));
-        cout<<numValue << endl;
-   }*/
-
-   cout << numValue << endl;
+   //cout << numValue << endl;
+   //stream << "0x" << std::hex << numValue << endl;
+   string valToSet;
+   //valToSet << "0x" << hex << numValue << endl;
 
    int fd, result, set;
 
    fd = wiringPiI2CSetup(0x28);
 
-   cout << "Init result: "<< fd << endl;
-
-   set = wiringPiI2CWriteReg8(fd, 0xaf, 0x3);
+   set = wiringPiI2CWriteReg8(fd, 0xaf, numValue);
   
 
 
@@ -43,27 +29,19 @@ int main(int argc, char *argv[])
    {
 
       fd = wiringPiI2CSetup(0x2f);
-      set = wiringPiI2CWriteReg8(fd, 0xaf, 0x3);
+      set = wiringPiI2CWriteReg8(fd, 0xaf, numValue);
       if(set == -1)
       {
 
 	fd = wiringPiI2CSetup(0x2e);
-      	set = wiringPiI2CWriteReg8(fd, 0xaf, 0x03);
+      	set = wiringPiI2CWriteReg8(fd, 0xaf, numValue);
       	if(set == -1)
       	{
       		cout << "Error.  Errno is: " << errno << endl;
 	}
-	else {
-		cout <<"sucess: "<<set<<endl;
-   	}
+	
 
       }
-      else {
-	cout <<"sucess: "<<set<<endl;
-      }
-   }
-   else {
-	cout <<"sucess: "<<set<<endl;
    }
 
 
@@ -74,7 +52,29 @@ int main(int argc, char *argv[])
       cout << "Error.  Errno is: " << errno << endl;
    }
    else {
-	cout <<"data: "<<result<<endl;
+	cout <<result<<endl;
    }
    
 }
+
+   //string word;
+   //cout << argv << endl;
+   //cout << argv[1] << endl;
+   //memcpy(word, argv[0])
+
+   //cout << num << endl;
+
+   //numValue = Integer.parseInt(word);
+   //cout <<"hi " << numValue << endl;
+
+   /*for (int i=0; i < argv[0].len(); i++) {
+   	char* value = argv[i];
+        cout << value << endl;
+   	numValue += (*value%48)*(10^(i-1));
+        cout<<numValue << endl;
+   }*/
+      //cout << "Init result: "<< fd << endl;
+
+   /*else {
+		cout <<"sucess: "<<set<<endl;
+   	}*/
